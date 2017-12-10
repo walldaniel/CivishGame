be73db3e4fedd1a9e19fc1d90aa3163fe5499438
Daniel_Wall <daniel.wall@ocsbstudent.ca>
2017-12-10 18:45:08 -0500

    initial commit, map works and can scroll around

---
+++ Civilization/desktop/src/com/wall/civilization/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.wall.civilization.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wall.civilization.Civ;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.foregroundFPS = 60;
+		new LwjglApplication(new Civ(), config);
+	}
+}
