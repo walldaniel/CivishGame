562322ce8c7a0575056ed061a0c151f9d99d38db
walldaniel <daniel.wall@ocsbstudent.ca>
2017-12-15 12:51:09 -0500

    changed map interface

--- core/src/com/wall/civilization/Civ.java
+++ core/src/com/wall/civilization/Civ.java
@@ -5,8 +5,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.MapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.wall.civilization.map.Map;
+import com.wall.civilization.map.Tile;
 
 public class Civ extends ApplicationAdapter {
 	public static final int SCREEN_WIDTH = 600;
@@ -17,13 +27,20 @@ public class Civ extends ApplicationAdapter {
 	private OrthographicCamera cam;
 
 	SpriteBatch sb;
-	Map map;
+	// Map map;
 
 	float x = 0;
 	float y = 0;
 
 	float mouseX = 0;
 	float mouseY = 0;
+
+	private TiledMap map;
+	private MapLayers layers;
+	private TiledMapTileLayer layer1;
+	private Texture tiles;
+	private TextureRegion region;
+	private OrthogonalTiledMapRenderer renderer;
 
 	@Override
 	public void create() {
@@ -33,38 +50,58 @@ public class Civ extends ApplicationAdapter {
 		cam = new OrthographicCamera(SCREEN_WIDTH, SCREEN_HEIGHT * (SCREEN_WIDTH / SCREEN_HEIGHT));
 		cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
 		cam.update();
+		cam.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-		map = new Map(MAP_SIZE, MAP_SIZE);
+		tiles = new Texture(Tile.PLAINS_PATH);
+		plains = new TextureRegion(tiles, 0, 0, 128, 128);
+		map = new TiledMap();
+		layers = map.getLayers();
+		layer1 = new TiledMapTileLayer(30, 30, 128, 128);
+
+		TiledMapTileLayer.Cell[] cells = { new TiledMapTileLayer.Cell(), new TiledMapTileLayer.Cell() };
+		cells[0].setTile(new StaticTiledMapTile(region));
+		cells[1].setTile(new StaticTiledMapTile(region));
+		layer1.setCell(0, 1, cells[0]);
+		for (int i = 1; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				layer1.setCell(i, j, cells[(int) (Math.random() * cells.length)]);
+			}
+		}
+
+		layers.add(layer1);
+
+		renderer = new OrthogonalTiledMapRenderer(map);
+		renderer.setView(cam);
+		// map = new Map(MAP_SIZE, MAP_SIZE);
 	}
 
 	public void update(float dt) {
-		// Arrow keys map move
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			x += dt * 125f * cam.zoom;
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			x -= dt * 125f * cam.zoom;
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-			y -= dt * 125f * cam.zoom;
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-			y += dt * 125f * cam.zoom;
+		// Get map movement, if mouse is activated don't let arrow keys move map
+		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+			cam.translate(-Gdx.input.getDeltaX() * cam.zoom, -Gdx.input.getDeltaY() * cam.zoom);
+		} else {
+			// Arrow keys map move
+			if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+				cam.translate(-150f * dt * cam.zoom, 0);
+			}
+			if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+				cam.translate(150f * dt * cam.zoom, 0);
+			}
+			if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+				cam.translate(0, -150f * dt * cam.zoom);
+			}
+			if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+				cam.translate(0, 150f * dt * cam.zoom);
+			}
 		}
 
-		// Get mouse scrolling
+		// Get zoom feature
 		if (Gdx.input.isKeyPressed(Input.Keys.EQUALS)) { // The equals key is same as plus
 			if (cam.zoom > 1f)
 				cam.zoom -= 0.03f;
 		} else if (Gdx.input.isKeyPressed(Input.Keys.MINUS)) {
-			if (cam.zoom < 6f)
+			if (cam.zoom < 50f)
 				cam.zoom += 0.03f;
-		}
-
-		// Get mouse movement
-		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-			x += Gdx.input.getDeltaX() * 2.1f * cam.zoom;
-			y -= Gdx.input.getDeltaY() * 2.1f * cam.zoom;
 		}
 
 		mouseX = Gdx.input.getX();
@@ -73,26 +110,21 @@ public class Civ extends ApplicationAdapter {
 
 	@Override
 	public void render() {
-		// Print out mouse coords
-		System.out.println("Tile: " + (int) ((mouseX - x - (128 * (cam.zoom - 1f))) / (128 / cam.zoom)) + ", "
-					+ (int) ((SCREEN_HEIGHT - mouseY - y - (128 * (cam.zoom - 1f))) / (128 / cam.zoom)));
-		
-		System.out.println(cam.zoom + " - " + mouseX + " - " + mouseY);
-		cam.zoom = 5f;
-		
-		// Clear the screen
-		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-		
-		sb.begin();
-		// Get updates
 		update(Gdx.graphics.getDeltaTime());
+		// Print out mouse coords
+		// System.out.println("Tile: " + (int) ((mouseX - x - (128 * (cam.zoom - 1f))) /
+		// (128 / cam.zoom)) + ", "
+		// + (int) ((SCREEN_HEIGHT - mouseY - y - (128 * (cam.zoom - 1f))) / (128 /
+		// cam.zoom)));
+		//
+		// System.out.println(cam.zoom + " - " + mouseX + " - " + mouseY);
+		Gdx.gl.glClearColor(100f / 255f, 100f / 255f, 250f / 255f, 1f);
+		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+
 		cam.update();
-		sb.setProjectionMatrix(cam.combined);
+		renderer.setView(cam);
+		renderer.render();
 
-		// Draw the map
-		map.drawMap(sb, x, y, Gdx.graphics.getDeltaTime());
-
-		sb.end();
 	}
 
 	@Override
